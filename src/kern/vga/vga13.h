4c20d1d8415056d59dd5380abbfbdd2664839509
Schuyler Martin <schuylermartin45@gmail.com>
2017-02-27 21:35:41 -0500

    Small notation change to header file

--- src/kern/vga/vga13.h
+++ src/kern/vga/vga13.h
@@ -18,14 +18,14 @@
 #include "vga.h"
 
 /** Macros     **/
-// Start and ending address of the memory space that Mode13h uses (64kB total)
-#define VGA13_MEM_SIZE   0xFA00
-#define VGA13_MEM_BEGIN  0xA0000
-#define VGA13_MEM_END    (VGA13_MEM_BEGIN + VGA13_MEM_SIZE)
-
 // Graphics mode screen dimensions (1 byte per pixel in memory)
 #define VGA13_WIDTH     320
 #define VGA13_HEIGHT    200
+// Start and ending address of the memory space that Mode13h uses (64kB total)
+#define VGA13_MEM_SIZE   (VGA13_WIDTH * VGA13_HEIGHT)
+#define VGA13_MEM_BEGIN  0xA0000
+#define VGA13_MEM_END    (VGA13_MEM_BEGIN + VGA13_MEM_SIZE)
+
 // size of Mode13h palette (color look-up table)
 #define VGA13_PALETTE_SIZE      256
 // reserved colors, and how many are reserved
