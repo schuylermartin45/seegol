bb127b0ff72a88f5c1d0b16e3d410da8e3646611
Schuyler Martin <schuylermartin45@gmail.com>
2017-03-05 11:54:42 -0500

    Started to procedurally draw stars

--- src/usr/trench_run.c
+++ src/usr/trench_run.c
@@ -27,6 +27,62 @@ void trench_run_init(Program* prog)
 }
 
 /*
+** Procedurally draw the stars above the trench
+**
+** @param seed Seed value to use in rendering
+** @param h_bound Max height to draw stars (keeps them above the trench)
+*/
+static void __trench_run_render_stars(uint16_t seed, uint16_t h_bound)
+{
+    // manipulate the seed a little bit...because why not?
+    seed += seed % 16;
+    seed += seed % 10;
+    seed += seed % 32;
+    // ensure a certain length
+    if (seed < 10)
+        seed *= 12;
+    if (seed < 100)
+        seed *= 222;
+    if (seed < 1000)
+        seed *= 1234;
+        
+    // use the seed to determine certain parameters
+    Point_2D cursor = {seed % gl_getw(), seed % 10};
+    uint16_t dx = (seed % 50) + 1;
+    uint16_t dy = ((seed % 4) + 1) * 3;
+
+    // counts the cycles
+    uint16_t cntr = 0;
+    // procedurally draw stars; attempting to make some amount of noise
+    // with all the variables at play; stars are drawn starting at the upper
+    // left corner and down until we hit a limit
+    while (cursor.y < h_bound)
+    {
+        gl_put_pixel(cursor, RGB_WHITE);
+        switch (cntr % 4)
+        {
+            case 0: cursor.x = cursor.x + dx;
+                break;
+            case 1: cursor.x = cursor.x + (((cntr % 2) + 1) * dx);
+                break;
+            case 2: cursor.x = cursor.x + (dy * dx);
+                break;
+            case 3: cursor.x = cursor.x + (dy / 3) + (((dx * dy) % cntr));
+                break;
+        }
+        if (cursor.x >= gl_getw())
+        {
+            cursor.x %= gl_getw();
+            if (cntr % 2)
+                cursor.y += dy;
+            else
+                cursor.y += (dy * 2) + 1;
+        }
+        ++cntr;
+    }
+}
+
+/*
 ** Procedurally draw the Death Star trench run
 **
 ** @param seed Seed value to use in rendering
@@ -43,6 +99,9 @@ static void __trench_run_render_frame(uint16_t seed)
     // box surrounding center point, "openning" to the trench
     Point_2D ctr_ul = {ctr_pt.x - (fr_w / 16), ctr_pt.y - (fr_h / 16)};
     Point_2D ctr_ll = {ctr_pt.x + (fr_w / 16), ctr_pt.y + (fr_h / 16)};
+
+    // generate stars
+    __trench_run_render_stars(seed, fr_h / 4);
 
     // "targetting computer" indicator
     gl_draw_strf(PT2(fr_w - (fr_w / 4), fr_h / 20), ROGUE_RED, ROGUE_YLW,
