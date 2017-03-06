33678bfa3562f0d18e4d3e8b842def22b964e276
Schuyler Martin <schuylermartin45@gmail.com>
2017-03-05 23:32:22 -0500

    Proton torpedos have just skimmed the surface
    - Through some real hacky code, the basic trench run program is
      "complete" but could be improved with real animations
    - I need a timer at this point

--- src/usr/trench_run.c
+++ src/usr/trench_run.c
@@ -27,21 +27,6 @@ void trench_run_init(Program* prog)
 }
 
 /*
-** Finds a point on a line, given an offset from the front
-**
-** @param p0 Starting point
-** @param p1 Ending point
-** @param perc Fraction of the line to find the point (0-10)
-** @param out Output point
-*/
-static void __trench_run_pt_on_line(Point_2D p0, Point_2D p1, uint8_t perc,
-    Point_2D* out)
-{
-    out->x = (p1.x - p0.x) / perc;
-    out->y = (p1.y - p0.y) / perc;
-}
-
-/*
 ** Procedurally draw the stars above the trench
 **
 ** @param seed Seed value to use in rendering
@@ -54,9 +39,9 @@ static void __trench_run_render_stars(uint16_t seed,
     Point_2D tr_ul, Point_2D ctr_ul, Point_2D ctr_ur, Point_2D tr_ur)
 {
     // manipulate the seed a little bit...because why not?
-    seed += seed % 16;
-    seed += seed % 10;
-    seed += seed % 32;
+    //seed += seed % 16;
+    //seed += seed % 10;
+    //seed += seed % 32;
     // ensure a certain length
     if (seed < 10)
         seed *= 12;
@@ -81,14 +66,18 @@ static void __trench_run_render_stars(uint16_t seed,
         if (cursor.y > tr_ul.y)
         {
             // solve the lines
+            // Form of equation in use: 2 point form
+            // x(dy) - y(dx) = x1y2 - x2y1; dy = y2-y1; dx = x2 - x1
+            // Upper-left top edge of the trench
             int16_t ul_dx = ctr_ul.x - tr_ul.x, ul_dy = ctr_ul.y - tr_ul.y;
-            int16_t ul_eq = (ul_dx * ctr_ul.x) - (ul_dy * ctr_ul.y);
-            int16_t cur_ul_eq = (ul_dx * cursor.x) - (ul_dy * cursor.y);
+            int16_t ul_eq = (tr_ul.x * ctr_ul.y) - (ctr_ul.x * tr_ul.y);
+            int16_t cur_ul_eq = (cursor.x * ul_dy) - (cursor.y * ul_dx);
+            // Upper-right top edge of the trench
             int16_t ur_dx = ctr_ur.x - tr_ur.x, ur_dy = ctr_ur.y - tr_ur.y;
-            int16_t ur_eq = (ul_dx * ctr_ur.x) - (ul_dy * ctr_ur.y);
-            int16_t cur_ur_eq = (ur_dx * cursor.x) - (ur_dy * cursor.y);
+            int16_t ur_eq = (tr_ur.x * ctr_ur.y) - (ctr_ur.x * tr_ur.y);
+            int16_t cur_ur_eq = (cursor.x * ur_dy) - (cursor.y * ur_dx);
             // see if we should draw this
-            if ((cur_ul_eq > ul_eq) && (cur_ur_eq > ur_eq))
+            if ((cur_ul_eq > ul_eq) && (cur_ur_eq < ur_eq))
                 gl_put_pixel(cursor, RGB_WHITE);
         }
         else
@@ -131,14 +120,14 @@ static void __trench_run_render_frame(uint16_t seed)
     Point_2D ctr_pt = {fr_w / 2, fr_h / 2};
     // box surrounding near plane of the image, the start of the corner lines
     Point_2D tr_ul = {0, fr_h / 4};
-    Point_2D tr_ll = {fr_w, fr_h};
+    Point_2D tr_lr = {fr_w, fr_h};
     // box surrounding center point, "openning" to the trench (far plane)
     Point_2D ctr_ul = {ctr_pt.x - (fr_w / 16), ctr_pt.y - (fr_h / 16)};
-    Point_2D ctr_ll = {ctr_pt.x + (fr_w / 16), ctr_pt.y + (fr_h / 16)};
+    Point_2D ctr_lr = {ctr_pt.x + (fr_w / 16), ctr_pt.y + (fr_h / 16)};
 
     // generate stars
     __trench_run_render_stars(seed, tr_ul, ctr_ul,
-        PT2(ctr_ll.x, ctr_ul.y), PT2(tr_ul.x, tr_ul.y));
+        PT2(ctr_lr.x, ctr_ul.y), PT2(tr_ul.x, tr_ul.y));
 
     // "targetting computer" indicator
     gl_draw_strf(PT2(fr_w - (fr_w / 4), fr_h / 20), ROGUE_RED, ROGUE_YLW,
@@ -147,26 +136,71 @@ static void __trench_run_render_frame(uint16_t seed)
     // draw the corners of the trenches, based on the screen dimensions
     // top corners of trench
     gl_draw_line(tr_ul, ctr_ul, ROGUE_YLW);
-    gl_draw_line(PT2(tr_ll.x, tr_ul.y), PT2(ctr_ll.x, ctr_ul.y), ROGUE_YLW);
+    gl_draw_line(PT2(tr_lr.x, tr_ul.y), PT2(ctr_lr.x, ctr_ul.y), ROGUE_YLW);
     // bottom corners of trench
-    gl_draw_line(PT2(tr_ul.x, tr_ll.y), PT2(ctr_ul.x, ctr_ll.y), ROGUE_YLW);
-    gl_draw_line(tr_ll, ctr_ll, ROGUE_YLW);
+    gl_draw_line(PT2(tr_ul.x, tr_lr.y), PT2(ctr_ul.x, ctr_lr.y), ROGUE_YLW);
+    gl_draw_line(tr_lr, ctr_lr, ROGUE_YLW);
 
     // bound the end of the trench
-    gl_draw_line(ctr_ul, PT2(ctr_ul.x, ctr_ll.y), ROGUE_YLW);
-    gl_draw_line(PT2(ctr_ul.x, ctr_ll.y), ctr_ll, ROGUE_YLW);
-    gl_draw_line(ctr_ll, PT2(ctr_ll.x, ctr_ul.y), ROGUE_YLW);
+    gl_draw_line(ctr_ul, PT2(ctr_ul.x, ctr_lr.y), ROGUE_YLW);
+    gl_draw_line(PT2(ctr_ul.x, ctr_lr.y), ctr_lr, ROGUE_YLW);
+    gl_draw_line(ctr_lr, PT2(ctr_lr.x, ctr_ul.y), ROGUE_YLW);
 
-    // TODO
     // trench lines determined by seed
-    Point_2D tr_p0;
-    Point_2D tr_p1;
-    __trench_run_pt_on_line(tr_ul, ctr_ul, 5, &tr_p0);
-    __trench_run_pt_on_line(PT2(tr_ul.x, tr_ll.y), PT2(ctr_ul.x, ctr_ul.y),
-        5, &tr_p1);
-    gl_draw_line(tr_p0, tr_p1, ROGUE_YLW);
-    __trench_run_pt_on_line(ctr_ll, tr_ll, 5, &tr_p0);
-    gl_draw_line(tr_p1, tr_p0, ROGUE_YLW);
+    for (uint8_t tr_lines=1; tr_lines<3; ++tr_lines)
+    {
+        uint16_t frac_w = ((seed % 2) + 1) * (fr_w / 6);
+        Point_2D tr_p0 = {frac_w * tr_lines, 0};
+        Point_2D tr_p1 = tr_p0;
+
+        // cheaty way; solving equations without decimals is hard
+        // ...high school geometry is also hard, for the record
+        RGB_8 cmp_color = {0,0,0};
+        // find the first 2 yellow pixels to draw the left vertical bar
+        uint16_t y = 0;
+        for(; y<fr_h; ++y)
+        {
+            tr_p0.y = y;
+            gl_get_pixel(tr_p0, &cmp_color);
+            if (vga_RGB_8_cmp(cmp_color, ROGUE_YLW))
+            {
+                ++y;
+                break;
+            }
+        }
+        for(; y<fr_h; ++y)
+        {
+            tr_p1.y = y;
+            gl_get_pixel(tr_p1, &cmp_color);
+            if (vga_RGB_8_cmp(cmp_color, ROGUE_YLW))
+                break;
+        }
+        gl_draw_line(tr_p0, tr_p1, ROGUE_YLW);
+        // finding the subsequent bars is easier since we know one dimension
+        // horizontal bar
+        tr_p0 = tr_p1;
+        // start looking for x past the center of the screen; this prevents
+        // issues with running into aliased values
+        for(uint16_t x=(fr_w / 2)+1; x<fr_w; ++x)
+        {
+            tr_p0.x = x;
+            gl_get_pixel(tr_p0, &cmp_color);
+            if (vga_RGB_8_cmp(cmp_color, ROGUE_YLW))
+                break;
+        }
+        gl_draw_line(tr_p1, tr_p0, ROGUE_YLW);
+        // last vertical bar
+        tr_p1 = tr_p0;
+        y = fr_h / 2;
+        for(; y<fr_h; --y)
+        {
+            tr_p1.y = y;
+            gl_get_pixel(tr_p1, &cmp_color);
+            if (vga_RGB_8_cmp(cmp_color, ROGUE_YLW))
+                break;
+        }
+        gl_draw_line(tr_p0, tr_p1, ROGUE_YLW);
+    }
 }
 
 /*
@@ -178,19 +212,20 @@ uint8_t trench_run_main(uint8_t argc, char* argv[])
     // default to VGA13
     gl_enter(VGA_MODE_13);
 
-    // TODO animate!
     // draw the plans w/ a seed
-    for(uint16_t t=0; t<1000; ++t)
+    for(uint16_t t=0; t<10; ++t)
     {
         __trench_run_render_frame(1342 + t);
+        kio_wait_key(KEY_SPACE);
+        gl_clrscr();
         // TODO get a real timer
-        //kio_wait_key(KEY_SPACE);
+        /*
         uint16_t time_block = 0;
         for(uint16_t s=0; s<65000; ++s)
             time_block += s;
-        gl_clrscr();
         for(uint16_t s=0; s<65000; ++s)
             time_block += s;
+        */
     }
 
     // clean-up and exit
